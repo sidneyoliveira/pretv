b1afcd9b329ae16bfb778ec27c51e40ae2fb4167
sidneyoliveira <sidneyoliveira2015@gmail.com>
2023-11-07 10:55:01 -0300

    alt

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,9 @@ export default function App() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide className='item-img'>
+          <img src="./img/a1.jpg" alt="" />
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
