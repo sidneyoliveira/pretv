b39f034256f1275a0c667f7c51d48e1c58fbe716
sidneyoliveira <sidneyoliveira2015@gmail.com>
2023-11-07 10:31:37 -0300

    alt

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById("app"));
+    
