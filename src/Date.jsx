d571d98649948d04aa229a71b4f682a0ed9e0abe
sidneyoliveira <sidneyoliveira2015@gmail.com>
2023-11-16 09:20:12 -0300

    a

---
+++ src/Date.jsx
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect, useState } from 'react';
+import './date.css';
+
+
+function DateTime() {
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    
+  }, [count]);
+ 
+    return (
+      <>
+          <div className="ImageSlide">
+          </div>
+      </>
+    );
+  }
+export default DateTime;
